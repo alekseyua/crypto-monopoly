5267d5873b9c60825cc23469ab5b962f86b62c23
alekseyua <alekseyuadnepr@gmail.com>
2025-11-07 19:57:24 +0200

    fix bug scale v2

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routers/roters.jsx';
+import { useEffect } from 'react';
 
 function App() {
+	useEffect(() => {
+		const preventZoom = (e: any) => {
+			if (e.touches.length > 1) e.preventDefault();
+		};
+		document.addEventListener("touchmove", preventZoom, { passive: false });
+		return () => document.removeEventListener("touchmove", preventZoom);
+	}, []);
+
 	return <RouterProvider router={router} />;
 }
 
