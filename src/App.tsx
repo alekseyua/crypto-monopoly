9678549cf1c554a1f3b37ee719826d981798a7da
alekseyua <alekseyuadnepr@gmail.com>
2025-11-07 19:52:27 +0200

    fix bug scale

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routers/roters.jsx';
-import { useEffect } from 'react';
 
 function App() {
-	// useEffect(()=>{
-	// 	document.
-	// })
 	return <RouterProvider router={router} />;
 }
 
