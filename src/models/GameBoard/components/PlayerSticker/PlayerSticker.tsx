8680719646a7876e254625591284e06f5022b684
Roman <rromann4k2030@gmail.com>
2025-12-08 01:55:29 +0000

    fix upscaling cardHeader and ladel

--- src/models/GameBoard/components/PlayerSticker/PlayerSticker.tsx
+++ src/models/GameBoard/components/PlayerSticker/PlayerSticker.tsx
@@ -37,7 +37,7 @@ const PlayerSticker: React.FC<IPlayerStickerProps> = ({
               background: player.color,
             }}
           >
-            <AvatarBlock avatar={player.avatar} color={player.color} width={15} height={15}/>
+            <AvatarBlock avatar={player.avatar} color={player.color} />
           </div>
         ))}
       </div>
